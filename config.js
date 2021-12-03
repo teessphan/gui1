2e4448920603e98628a1b47c18a2edd7c8f704c9
teessphan <95341054+teessphan@users.noreply.github.com>
2021-12-03 15:07:56 +0700

    ry

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Gửi đến Duy",
+    introTitle: 'Phạm Khánh Duy',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^HiHi^^',
+    title: 'Cậu kia! Có yêu mình không!! 🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Không hề',
+    btnNo: 'Yêu nha :3',
+    question: 'Thế là sao? Bạn nói gì đi..',
+    btnReply: 'Gửi cho mình <3',
+    reply: 'Mình ghét bạn :<<',
+    mess: 'Ơ kìa, thế mà bảo yêu mình, bảo thương mình😭',
+    messDesc: 'Dỗi rồi 😩😒',
+    btnAccept: 'Dỗ mình đi',
+    messLink: 'https://github.com/teessphan/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
